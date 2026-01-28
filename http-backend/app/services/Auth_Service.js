d647e8d0110ba62f5d2271eedddeb6337f27036b
Kiran <kiran.sadara6730@gmail.com>
2026-01-28 16:58:27 +0530

    dotenv is missing

--- http-backend/app/services/Auth_Service.js
+++ http-backend/app/services/Auth_Service.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
 import dotenv from 'dotenv'
+import e from 'express';
 dotenv.config({ path: "../.env" });
 
 const JWT_SECRET = process.env.JWT_SECRET
@@ -11,13 +12,17 @@ export const SigninService = async(email ,password)=>{
       
     try{
 
+
         const find_user = await Users.findOne({email  :email});
+        console.log("Found user: ", find_user);
 
         if(!find_user){
              throw new Error("Credentails Wrong")
         }
         
-        const password_check = await bcrypt.compare(find_user.password  , password);
+        const password_check = await bcrypt.compare(password  , find_user.password);
+
+        console.log("Password check: ", password_check);
 
         if(!password_check){
             throw new Error('Credentails Wrong')
